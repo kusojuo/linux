796c4ea10fb13178a17b3eccd2d0ebafdf4cdb15
Russ Dill <Russ.Dill@ti.com>
2013-07-16 22:07:20 -0700

    ARM: OMAP2+: AM33XX: Make sleep actions configurable.
    This patch makes sleep actions in sleep33xx.S configurable. This is
    necessary to allow the cpuidle to share the same code. It allows
    different actions to be taken depending on the desired target state.
    
    Signed-off-by: Russ Dill <russ.dill@ti.com>

--- arch/arm/mach-omap2/pm33xx.h
+++ arch/arm/mach-omap2/pm33xx.h
@@ -37,7 +37,7 @@ struct am33xx_pm_context {
  */
 struct am33xx_suspend_params {
 	void __iomem *emif_addr_virt;
-	u32 mem_type;
+	u32 wfi_flags;
 	void __iomem *dram_sync;
 };
 
@@ -73,5 +73,13 @@ void wkup_m3_register_txev_handler(void (*txev_handler)(void));
 #define AM33XX_EMIF_BASE		0x4C000000
 
 #define MEM_TYPE_DDR2		2
+#define MEM_TYPE_DDR3		3
+
+#define WFI_MEM_TYPE_DDR2	(1 << 0)
+#define WFI_MEM_TYPE_DDR3	(1 << 1)
+#define WFI_SELF_REFRESH	(1 << 2)
+#define WFI_SAVE_EMIF		(1 << 3)
+#define WFI_WAKE_M3		(1 << 4)
+#define WFI_SAVE_MPU		(1 << 5)
 
 #endif
